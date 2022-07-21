64fe310330f636f7e4377c688f48dc0e7f5b38f0
tanachok12 <tanachok_d@elearning.ac.th>
2022-07-21 23:25:54 +0700

    4.12

---
+++ lab03/src/components/Categories.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.category }} {{ event.organizer }} </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  //   data() {
+  //     return {
+  //       event: {
+  //         id: 5928101,
+  //         category: 'animal welfare',
+  //         title: 'Cat Adoption Day',
+  //         description: 'Find your new feline friend at this event.',
+  //         location: 'Meow Town',
+  //         date: 'January 28, 2022',
+  //         time: '12.00',
+  //         petsAllowed: true,
+  //         organizer: 'Kat Laydee'
+  //       }
+  //     }
+  //   }
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+h4 {
+  font-size: 20px;
+}
+</style>
